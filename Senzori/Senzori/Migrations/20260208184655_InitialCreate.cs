f491e535e8c39b8f0cee9855028f90e4e3639752
Dzenan Kazazovic <dzenan.kazazovic006@gmail.com>
2026-02-08 19:53:39 +0100

    feat: Implementing TPT Database scheme using EF Core

---
+++ Senzori/Senzori/Migrations/20260208184655_InitialCreate.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Senzori.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Devices",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Devices", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Controllers",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false),
+                    ModelKontrolera = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Status = table.Column<bool>(type: "bit", nullable: false),
+                    BrojKanala = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Controllers", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Controllers_Devices_Id",
+                        column: x => x.Id,
+                        principalTable: "Devices",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Senzors",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false),
+                    grad = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Vrijednost = table.Column<double>(type: "float", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Senzors", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Senzors_Devices_Id",
+                        column: x => x.Id,
+                        principalTable: "Devices",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Controllers");
+
+            migrationBuilder.DropTable(
+                name: "Senzors");
+
+            migrationBuilder.DropTable(
+                name: "Devices");
+        }
+    }
+}
