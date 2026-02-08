ef5501bc398b39fd75c3f1b23e2f22b3ed2b4482
Dzenan Kazazovic <dzenan.kazazovic006@gmail.com>
2026-02-08 14:53:46 +0100

    Implementing TPT database scheme using EF Core

---
+++ BookStore/BookStore/Data/BookStoreContext.cs
@@ -0,0 +1,31 @@
+﻿
+
+using Microsoft.EntityFrameworkCore;
+
+public class BookStoreContext : DbContext
+{
+  public DbSet<BibliotekaArtikal> StoreItems { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        // Table per Hierarchy (TPH) is default ef core option (one big table with Discriminator for recognising objects)
+        // but in that case we don't have normalized database so I use
+        // Table per type (TPT) so I can have normalized database. In this scenario we have to create table for every object
+        // and ef core can't just use select, it has to use select with join so it can show all data
+        // It is little bit more complex to implement but better for bigger solutions because of normalization and avoiding null fields in tables
+       
+
+        modelBuilder.Entity<BibliotekaArtikal>().ToTable("BookStoreItems");
+        modelBuilder.Entity<Knjiga>().ToTable("Books");
+        modelBuilder.Entity<Film>().ToTable("Movies");
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Server = (localdb)\\mssqllocaldb; Database = BookStoreDatabase; Trusted_Connection = True; "); 
+    }
+}
+
+
+
+
---
+++ BookStore/BookStore/Migrations/20260208133047_InitialCreate.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BIblioteka.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "BookStoreItems",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Naslov = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    GodinaIzdanja = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BookStoreItems", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Books",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false),
+                    Autor = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    BrojStranica = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Books", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Books_BookStoreItems_Id",
+                        column: x => x.Id,
+                        principalTable: "BookStoreItems",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Movies",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false),
+                    Reziser = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    TrajanjeUMinutama = table.Column<double>(type: "float", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Movies", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Movies_BookStoreItems_Id",
+                        column: x => x.Id,
+                        principalTable: "BookStoreItems",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Books");
+
+            migrationBuilder.DropTable(
+                name: "Movies");
+
+            migrationBuilder.DropTable(
+                name: "BookStoreItems");
+        }
+    }
+}
